5f38c3a2743abd44d10f561ecae3b654f8bf474f
ahmetoztrk <ahmetoztrk1709@gmail.com>
2022-02-25 23:02:34 +0300

    Added static/dynamic files

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  //res.sendFile(path.resolve(__dirname, "views/index.html"));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
